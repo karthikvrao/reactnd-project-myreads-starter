c0a9ccbb6675cba2b50f16f1af72e8e40e1f10b7
Karthik Rao <karthikvrao88@gmail.com>
2018-02-02 12:25:58 +0530

    refactor: Replace static HTML with Book component in ListShelves

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Book extends Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired
+  };
   render() {
+    const book = this.props.book;
     return (
       <div className="book">
         <div className="book-top">
@@ -10,12 +15,11 @@ class Book extends Component {
             style={{
               width: 128,
               height: 193,
-              backgroundImage:
-                'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
+              backgroundImage: `url(${book.imageLinks.thumbnail})`
             }}
           />
           <div className="book-shelf-changer">
-            <select>
+            <select defaultValue={book.shelf}>
               <option value="none" disabled>
                 Move to...
               </option>
@@ -26,8 +30,9 @@ class Book extends Component {
             </select>
           </div>
         </div>
-        <div className="book-title">To Kill a Mockingbird</div>
-        <div className="book-authors">Harper Lee</div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors.join(", ")}</div>
+        <div className="book-page-count">{book.pageCount} pages</div>
       </div>
     );
   }
